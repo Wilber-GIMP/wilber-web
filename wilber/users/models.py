140246161d7281f65e8697b7aeeeeff5e6a885af
Darpan Deva <flp9001@gmail.com>
2019-03-15 23:35:16 -0300

    function name of user

--- wilber/users/models.py
+++ wilber/users/models.py
@@ -28,7 +28,7 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-         
+
     def create_user(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
@@ -44,14 +44,14 @@ class UserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self._create_user(username, email, password, **extra_fields)
-        
-        
+
+
 class User(AbstractBaseUser, PermissionsMixin):
-    
+
     username_validator = UnicodeUsernameValidator()
-    
-    username = models.CharField(_("username"), max_length=15, unique=True, 
-        help_text=_("Required. 15 characters or fewer. Letters, \ numbers and @/./+/-/_ characters"), 
+
+    username = models.CharField(_("username"), max_length=15, unique=True,
+        help_text=_("Required. 15 characters or fewer. Letters, \ numbers and @/./+/-/_ characters"),
         validators=[username_validator],
         error_messages={
             'unique': _("A user with that username already exists."),
@@ -65,25 +65,28 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(_('active'), default=True, help_text=_('Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
     is_trusty = models.BooleanField(_('trusty'), default=False, help_text=_('Designates whether this user has confirmed his account.'))
-    
-    
+
+
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
-    
+
     objects = UserManager()
-    
+
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        
+
     def get_full_name(self):
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
-        
+
     def get_short_name(self):
         return self.first_name
-        
+
+    def name(self):
+        return self.get_full_name()
+
     def email_user(self, subject, message, from_email=None):
         send_mail(subject, message, from_email, [self.email])
 
@@ -91,35 +94,35 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    
+
     photo = models.ImageField(verbose_name=_("Profile Picture"), upload_to="profiles", default = 'profiles/none/no-img.jpg', max_length=255, null=True, blank=True)
-    
+
     phone = models.CharField(max_length=20, blank=True, default='')
     bio = models.TextField(default='', blank=True)
     organization = models.CharField(max_length=100, default='', blank=True)
-    
+
     website = models.URLField(default='', blank=True)
     facebook = models.URLField(default='', blank=True)
     instagram = models.URLField(default='', blank=True)
-    
+
     birthday = models.DateField(null=True, blank=True)
-    
+
     city = models.CharField(max_length=100, default='', blank=True)
     country = models.CharField(max_length=100, default='', blank=True)
-    
+
     def get_absolute_url(self):
         return reverse('user:detail', kwargs={'username': self.user.username})
-    
+
     def __str__(self):
         return self.user.username
-        
-    
+
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-    
 
 
-@receiver(post_save, sender=User)    
+
+@receiver(post_save, sender=User)
 def create_profile(sender, **kwargs):
     user = kwargs["instance"]
     if kwargs["created"]:
