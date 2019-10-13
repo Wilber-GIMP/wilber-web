63c1ca8aaba763b31c62d9cd5445a79b93800e0d
Darpan Deva <flp9001@gmail.com>
2019-10-13 19:58:23 -0300

    Staticfiles folder change

--- wilber/config/settings/base.py
+++ wilber/config/settings/base.py
@@ -201,7 +201,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 FILE_UPLOAD_PERMISSIONS = 0o644
