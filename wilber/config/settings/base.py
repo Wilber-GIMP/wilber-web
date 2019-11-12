bf320cfad1c3c71b73112801cfd539c56a5a5a48
Darpan Deva <flp9001@gmail.com>
2019-11-12 01:21:41 -0300

    Facebook locale

--- wilber/config/settings/base.py
+++ wilber/config/settings/base.py
@@ -205,7 +205,7 @@ SOCIALACCOUNT_PROVIDERS = {
             'updated_time',
         ],
         'EXCHANGE_TOKEN': True,
-        'LOCALE_FUNC': 'path.to.callable',
+        'LOCALE_FUNC': lambda request: 'en_US',
         'VERIFIED_EMAIL': False,
         'VERSION': 'v2.12',
     },
